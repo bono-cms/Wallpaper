b14bb540bfd4a75a54898984b0fb10e84604bcdf
dave-yang <daworld.ny@gmail.com>
2021-04-18 20:25:50 +0500

    Create Dropper.php

---
+++ Storage/MySQL/Dropper.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Wallpaper\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractStorageDropper;
+
+final class Dropper extends AbstractStorageDropper
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTables()
+    {
+        return [
+            WallpaperMapper::getTableName(),
+            WallpaperTranslationMapper::getTableName()
+        ];
+    }
+}
