575ded4c39c3a7a2826cdcc259b1dd26a4759bcb
dave-yang <daworld.ny@gmail.com>
2021-04-20 00:34:44 +0500

    Initial Wallpaper controller

---
+++ Controller/Wallpaper.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Wallpaper\Controller;
+
+use Site\Controller\AbstractController;
+
+final class Wallpaper extends AbstractController
+{
+    /**
+     * Renders single wallpaper
+     * 
+     * @param int $id Wallpaper id
+     * @return string
+     */
+    public function viewAction($id)
+    {
+        $wallpaper = $this->getModuleService('wallpaperService')->fetchById($id, false);
+
+        if ($wallpaper){
+            // ...
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Renders filter page
+     * 
+     * @return string
+     */
+    public function filterAction()
+    {
+        $filter = $this->request->getQuery('filter', []);
+
+        // ...
+    }
+}
