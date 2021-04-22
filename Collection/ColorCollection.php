541ba810ce0f311d6afb527c2fb26f61222fc9ea
dave-yang <daworld.ny@gmail.com>
2021-04-22 19:20:34 +0500

    Added ColorCollection

---
+++ Collection/ColorCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Shop\Collection;
+
+use Krystal\Stdlib\ArrayCollection;
+
+final class ColorCollection extends ArrayCollection
+{
+    /* Color constants */
+    const WHITE = 1;
+    const BLACK = 2;
+    const BLUE = 3;
+    const RED = 4;
+    const YELLOW = 5;
+    const BROWN = 6;
+    const PURPLE = 7;
+}
